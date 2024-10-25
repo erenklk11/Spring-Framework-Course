58e91e37e605399328c3d8f4b4c00d89c6767b7c
erenklk11 <erenklk11@gmail.com>
2024-10-25 23:03:03 +0300

    Gained my first front-end experience using Spring MVC and Thymeleaf

---
+++ 05-spring-mvc-thymeleaf/src/main/java/com/spring/course/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,46 @@
+package com.spring.course.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String processFormVersionTwo(HttpServletRequest request, Model model) {
+
+        String name = request.getParameter("studentName");
+        name = name.toUpperCase();
+
+        String result = "Yo! " + name;
+
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String name, Model model) {
+
+        name = name.toUpperCase();
+
+        String result = "Yo! " + name;
+
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+}
---
+++ 05-spring-mvc-thymeleaf/src/test/java/com/spring/course/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.course.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
